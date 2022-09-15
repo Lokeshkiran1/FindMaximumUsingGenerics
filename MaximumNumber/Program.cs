b1a3939e8f6ffbc33fc3cca28bf27c3bbdb44325
LokeshKiran <lokeshkskiran@gmail.com>
2022-09-15 11:37:49 +0530

    [Lokesh]Added:Maximum string identification

--- MaximumNumber/Program.cs
+++ MaximumNumber/Program.cs
@@ -12,7 +12,8 @@ namespace MaximumNumber
         {
             Console.WriteLine("welcome to maximum number identification: ");
             MaximumOfThreeNumbers maximumOfThreeNumbers = new MaximumOfThreeNumbers();
-            Console.WriteLine("enter option:\n1.Maximum of integers\n2.Maximum among floating values");
+            Console.WriteLine("enter option:\n1.Maximum of integers\n2.Maximum among floating values\n" +
+                "3.Maximum of string");
             int option = Convert.ToInt32(Console.ReadLine());
 
            switch(option)
@@ -24,6 +25,10 @@ namespace MaximumNumber
                     case 2:
                     double var1 = maximumOfThreeNumbers.MaximumFloatNumber(23.255, 35.3669, 25.5566);
                     Console.WriteLine("Maximum float number : "+var1);
+                    break;
+                case 3:
+                    string str = maximumOfThreeNumbers.MaximumStringNumber("lokesh","Kiran","Lokeshks");
+                    Console.WriteLine("maximum string is : "+str);
                     break;
                     default:
                     Console.WriteLine(" wrong option");
