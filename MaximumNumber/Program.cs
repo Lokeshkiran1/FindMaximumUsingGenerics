899d213944a656d9e5e98f28b4638f06e7d04f56
LokeshKiran <lokeshkskiran@gmail.com>
2022-09-15 11:30:54 +0530

    [Lokesh]Added:Maximum number identification among three floating values

--- MaximumNumber/Program.cs
+++ MaximumNumber/Program.cs
@@ -10,9 +10,25 @@ namespace MaximumNumber
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("welcome to maximum number identification: ");
             MaximumOfThreeNumbers maximumOfThreeNumbers = new MaximumOfThreeNumbers();
-            int var=maximumOfThreeNumbers.PrintMaximumNumber(20,10,30);
-            Console.WriteLine("maximum of three numbers: "+var);
+            Console.WriteLine("enter option:\n1.Maximum of integers\n2.Maximum among floating values");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+           switch(option)
+            {
+                case 1:
+                    int var = maximumOfThreeNumbers.PrintMaximumNumber(20, 10, 30);
+                    Console.WriteLine("maximum of three numbers: " + var);
+                    break;
+                    case 2:
+                    double var1 = maximumOfThreeNumbers.MaximumFloatNumber(23.255, 35.3669, 25.5566);
+                    Console.WriteLine("Maximum float number : "+var1);
+                    break;
+                    default:
+                    Console.WriteLine(" wrong option");
+                    break;
+            }
         }
     }
 }
