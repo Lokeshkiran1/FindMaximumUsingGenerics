9654d21572100f3e305a4f46926117b4f4cebd45
LokeshKiran <lokeshkskiran@gmail.com>
2022-09-15 11:09:13 +0530

    [Lokesh]Added:Maximum number identification among three

---
+++ MaximumNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MaximumOfThreeNumbers maximumOfThreeNumbers = new MaximumOfThreeNumbers();
+            int var=maximumOfThreeNumbers.PrintMaximumNumber(20,10,30);
+            Console.WriteLine("maximum of three numbers: "+var);
+        }
+    }
+}
